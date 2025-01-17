8db09348d05b95dc9bca7a0b51fbcb34d9e30363
Aniketh <techrunstheworld@gmail.com>
2025-01-17 11:07:56 +0530

    feat: basics

---
+++ basics/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let mut x =5;
+
+    const THREE_HOURS_MINUTES:i32= 60*60*3;
+
+
+    println!("the value is :{THREE_HOURS_MINUTES}");
+
+
+    println!("the value of x is :{}",x);
+    x = 5;
+    println!("the value of x is :{x}");
+}
+
