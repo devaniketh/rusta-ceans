f28a2893881b1253bd7a23e3c4c6ecb091d2179b
Aniketh <techrunstheworld@gmail.com>
2025-01-17 14:02:53 +0530

    feat: booleans-controlflow-tuples

--- basics/src/main.rs
+++ basics/src/main.rs
@@ -1,3 +1,4 @@
+// variables
 fn main() {
     let mut x =5;
 
@@ -12,3 +13,56 @@ fn main() {
     println!("the value of x is :{x}");
 }
 
+
+fn main(){
+    let x = 5;
+
+    let x = x +1;
+    {
+        let x = x*2;
+        println!("The value of inner loop: {x}")
+
+    }  
+ 
+
+ //booleans
+fn main() {
+    let x = true;
+    let f :bool = false;
+    println!("vale of x: {x}");
+    println!("value of f: {f}");
+
+}
+//tuple
+fn main () {
+
+    let tup :(i32,f64,u8) =(500,5.6,2);
+
+    let  (_x,_y,_z)= tup;
+
+    println!("The values in the tuples: {_y}",);
+}
+
+// functions
+
+fn num (x:i32) ->i32{
+    x+1
+}
+
+fn main(){
+    let x = num(2);
+    println!("the value is :{x}");
+}
+
+
+conttrol flow 
+fn main (){
+    let num = 3;
+    if num<5{
+        println!("true statement ");
+
+    }
+    else {
+        println!("False statement")
+    }
+}
